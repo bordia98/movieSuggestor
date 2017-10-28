85252c0ee3d9575ad2dd68e0fa9cfea815f11772
bhavya bordia <bhavyabordia@gmail.com>
2017-10-28 23:23:46 +0530

    project completed

---
+++ MovieSuggestor/src/moviesuggestor/MovieSuggestor.java
@@ -0,0 +1,10 @@
+
+package moviesuggestor;
+
+public class MovieSuggestor {
+
+    public static void main(String[] args) {
+        openingpage form = new openingpage();
+        form.setVisible(true);    
+    }
+ }
